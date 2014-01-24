712cef8d3cecc8ce84ab30f205eb243dda1e6cf8
kikimosha <eszmvcu@mail.ru>
2014-01-24 22:05:26 +0200

    stop work calculator

--- include/pages/service/calculator.inc.php
+++ include/pages/service/calculator.inc.php
@@ -8,19 +8,23 @@ require_once(CLASS_DIR . '/tools.class.php');
 
 function getCalcVal ($array,$currency) {
     foreach ($array as $item) {
-        if($currency == $item[0])
-            return $item;
+        if($currency == $item['primarycode']) {
+            return array(
+                'price' => $item['recenttrades'][0]['price'],
+                'difficulty' => 0.000,
+                'reward' => 15
+            );
+        }
     }
 }
 
 $calc = array();
 
 $aData = $tools->getApi($config['price']['url'], $config['price']['target']);
-var_dump($aData);die;
-$calc['DGC'] = getCalcVal($aData, 'DGC');
-$calc['EAC'] = getCalcVal($aData, 'EAC');
-$calc['DOGE'] = getCalcVal($aData, 'DOGE');
-$calc['FST'] = getCalcVal($aData, 'FST');
+$calc['DGC'] = getCalcVal($aData['return']['markets'], 'DGC');
+$calc['EAC'] = getCalcVal($aData['return']['markets'], 'EAC');
+$calc['DOGE'] = getCalcVal($aData['return']['markets'], 'DOGE');
+$calc['FST'] = getCalcVal($aData['return']['markets'], 'FST');
 
 $smarty->assign("CALC", $calc);
 $smarty->assign("CONTENT", "default.tpl");
